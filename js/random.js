293340a39fda88d2f51ac1b6e2001a4e342d1c52
107405008 <102597886+107405008@users.noreply.github.com>
2023-04-19 16:38:46 +0800

    Update random.js
    使其隨機導向之網頁不包含自身

--- js/random.js
+++ js/random.js
@@ -1,12 +1,17 @@
-url = new Array();
+var currentFileName = window.location.pathname.split('/').pop();
+var ranNum;
+var fileName;
 
-for (let x = 0; x < 11; x++){
-    var ranNum =Math.floor(Math.random() * 11 ) ;
-}
-var i = ranNum
-console.log(ranNum)
+do {
+    ranNum =Math.floor(Math.random() * 11 ) ;
+    fileName = ranNum + ".html";
+
+} while (fileName === currentFileName);
+
+url = new Array();
+var i = ranNum;
 
 url[i] = "./" + i + ".html";
 
 str = "<a id='linkID' href='" + url[i] + "'>";
-document.write(str + "點我看隨機</a><br>");
+document.write(str + "點我一下</a><br>");
